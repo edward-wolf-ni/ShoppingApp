c033698701de2d81379f43d91010cad9729e925a
edward-wolf-ni <105945691+edward-wolf-ni@users.noreply.github.com>
2022-12-18 20:33:08 -0600

    add new dot

--- pages/404.js
+++ pages/404.js
@@ -1,6 +1,6 @@
 const FourOhFour = () => {
     return (
-      <h2 className="text-center font-bold text-5xl text-slate-500 py-[30px]">Page not found</h2>
+      <h2 className="text-center font-bold text-5xl text-slate-500 py-[30px]">Page not found...</h2>
     );
   };
   
